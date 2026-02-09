ee5b3805d26f6620777e1a14447a25d2c6549987
Arturo Grottoli <arturogrottoli@gmail.com>
2026-02-09 20:49:28 -0300

    new icon 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,18 +18,8 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
+      { url: '/icon.svg', type: 'image/svg+xml' },
+      { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
     ],
     apple: '/apple-icon.png',
   },
